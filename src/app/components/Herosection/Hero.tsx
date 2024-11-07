a44c244e5b4a1ad9df14e8c1b32aa49ce3f32879
iqrakalhoro <165007795+iqrakalhoro@users.noreply.github.com>
2024-11-08 01:48:49 +0500

    Nextjs Assignment 5

--- src/app/components/Herosection/Hero.tsx
+++ src/app/components/Herosection/Hero.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import {Libre_Bodoni} from "next/font/google";
+import {Libre_Bodoni} from 'next/font/google';
 import Image from 'next/image';
-// import img from './assets/image.jpeg';
-
 
 const LibreBodoni = Libre_Bodoni ({weight: '700'})
 
@@ -27,35 +25,4 @@ const Hero = () => {
 }
 
 export default Hero
-
-//   <div className="min-h-screen w-full flex flex-col">
-      
-//   {/* Main Content */}
-//   <div className="flex-grow flex  bg-white">
-//     {/* Left Container */}
-//     <div className="left-container h-screen flex items-center w-1/2 pl-12 justify-center">
-//       <div className="ml-8">
-//         <h1 className={`${LibreBodoni.className} "intro text-5xl text-[#000000] leading-4"`}>
-//          IMPECCABLE CRAFTSMANSHIP AND FINESSE
-//         </h1>
-
-//         <p className={`${LibreBodoni.className}"detail text-[1.1rem] text-[#787054] max-w-[38rem] my-[1.1rem] mb-12 leading-[1.8rem]"`}>
-//         An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.
-//         </p>
-//         <div className="btn-container flex space-x-20 justify-center">
-//           <button className="border border-[#A29875] text-white bg-[#A29875] px-6 py-3 rounded-lg transition duration-500 ">
-//           ExploreNow
-//           </button>
-       
-//         </div>ye code comment krun khud se krun sara
-//       </div>
-//     </div>
-//     {/* Right Container */}
-//     <div className="right-container h-screen flex items-center justify-center w-[50%] " >
-//       <div className="shadow-box flex justify-center items-center w-[60%]" >
-//         {/* <Image src={img} alt='' className='w-[370px] h-[370px] '></Image> */}
-//       </div>
-//     </div>
-//   </div>
-//   </div>
 
